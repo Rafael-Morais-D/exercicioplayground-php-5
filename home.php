960f6ec03201180336298473e6976c54a157b478
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-27 11:09:21 -0300

    Exercício sobre funções

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+
+    function cumprimentar() {
+        //Bloco de código
+        return "Olá mundo! <br>";
+    }
+
+    function completar() {
+        echo "Olá essa é a função completar <br>";
+        cumprimentar();
+    }
+
+    completar();
+
+?>
